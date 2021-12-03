1818414af9483efc6adf64418462cc56cbc1fbdd
Anselme DONATO <anselme.donato@telecom-paris.fr>
2021-12-03 17:04:10 +0100

    (Putting back the "Esc to quit")

--- moncraft/main.cpp
+++ moncraft/main.cpp
@@ -28,7 +28,6 @@ const char *fragmentShaderSource = "#version 330 core \n"
 int main()
 {
     // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -39,7 +38,6 @@ int main()
 #endif
 
     // glfw window creation
-    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -51,7 +49,6 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: load all OpenGL function pointers
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -144,17 +141,16 @@ int main()
     //Once we've linked the shaders into the program object, we no longer need them anymore
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+    
+    /* --------------------------------------------*/
 
     // render loop
-    // -----------
     while (!glfwWindowShouldClose(window))
     {
         // input
-        // -----
-        //processInput(window);
+        processInput(window);
 
         // render
-        // ------
         //glClearColor(0.94f, 0.59f, 0.71f, 1.0f);
         glClearColor(245.f/255, 214.f/255, 175.f/255, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -165,20 +161,17 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwDestroyWindow(window);
     glfwTerminate();
     return EXIT_SUCCESS;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -186,7 +179,6 @@ void processInput(GLFWwindow *window)
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and
